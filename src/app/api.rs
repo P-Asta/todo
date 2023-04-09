0219393aafc4552a3583b52e90a56e8d1a262c96
5-23 <yhanbyeol6@gmail.com>
2023-04-09 23:33:03 +0900

    static file test

--- src/app/api.rs
+++ src/app/api.rs
@@ -3,5 +3,6 @@ use std::fs;
 use axum::{extract::Path, response::Html};
 
 pub async fn asset(Path(name): Path<String>) -> Html<String>{
+    println!("{name}");
     Html(fs::read_to_string(format!("assets/{name}")).unwrap_or("404 Not Found".to_string()))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,28 @@
 mod app;
-use axum::{*, routing::{get, post}};
+use axum::{*, routing::{get, post}, http::StatusCode, handler::HandlerWithoutStateExt};
 use app::*;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
     let api = Router::new()
-        .route("/assets/:name", get(api::asset))
-        .route("/assets/:name", post(api::asset))
+        .route("/assets/*name", get(api::asset))
+        .route("/assets/*name", post(api::asset))
         ;
 
+    async fn handle_404() -> (StatusCode, &'static str) {
+        (StatusCode::NOT_FOUND, "Not found")
+    }
+    
+    let serve_dir = ServeDir::new("/assets").not_found_service(handle_404.into_service());
     let app = Router::new()
         .nest("/api", api)
         .route("/", get(front::index))
         .route("/todo/:name", get(front::todo_app))
         .route("/sign_up", get(front::sign_up))
-        // .route("/sign_upa", get(back::sign_up))
         .route("/sign_up", post(back::sign_up))
+        .fallback_service(serve_dir)
+    
         ;
 
     let server = Server::bind(&"0.0.0.0:5023".parse().unwrap())
