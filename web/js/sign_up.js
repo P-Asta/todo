6d38fead0581908323361d1582c5694cf0c05e85
5-23 <yhanbyeol6@gmail.com>
2023-04-10 19:23:52 +0900

    change todo!.svg

--- web/js/sign_up.js
+++ web/js/sign_up.js
@@ -3,6 +3,5 @@ document.getElementsByName("name")[0].value = q.get("n")
 document.getElementsByName("pw")[0].value   = q.get("p")
 document.getElementsByName("rpw")[0].value  = q.get("r")
 switch ({{error}}){
-    case 0: location.href = "/";break;
     case 1: alert("password가 일치하지 않습니다.")
 }
