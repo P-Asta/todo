7d3ccc5c3ed9f55689192980dfd2f6364bd10d3b
5-23 <yhanbyeol6@gmail.com>
2023-04-10 18:09:44 +0900

    change post

--- web/js/todo.js
+++ web/js/todo.js
@@ -9,10 +9,18 @@ class Post extends HTMLElement {
             fc: font color
             bc: background color
         */
+        // this.innerHTML = `
+        //     <article style="${ok? '--bc: #faffbf; --fc: #000': '--bg: #fff; --fc: #bbb'}">
+        //         <h1>${title}</h1>
+        //         <hr/>
+        //         <section>${discription}</section>
+        //     </article>
+        // `
+
         this.innerHTML = `
-            <article style="${ok? '--bc: #faffbf; --fc: #000': '--bg: #fff; --fc: #bbb'}">
+            <article style="${ok? '--fc: #000': '--fc: #000'}">
+                <img src="${ok?'https://raw.githubusercontent.com/5-23/todo/d8c5d94ed9656877ef38232bd320cd1eeaf5b4d0/assets/post_ok.svg': 'https://raw.githubusercontent.com/5-23/todo/d8c5d94ed9656877ef38232bd320cd1eeaf5b4d0/assets/post.svg'}"></img>
                 <h1>${title}</h1>
-                <hr/>
                 <section>${discription}</section>
             </article>
         `
